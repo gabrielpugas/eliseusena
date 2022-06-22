7ac899790451009e704f617cb263da8d3f98c8d2
gabrielpugas <gabrielpugas@gmail.com>
2022-06-22 08:42:21 -0300

    index

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div>
+      <h1>Eliseu e Cris, sábado tem gravação!</h1>
+      <h2>O site já está no ar, só falta fazer o site kkk</h2>
+      <h3>
+        Chama a sua véinha que vou chamar a minha, pra gente gravar as
+        propagandas
+      </h3>
+      
+    </div>
+  )
+}
+
+function Alert() {
+  alert('Presença confirmada!')
+}
+
+export default Home
