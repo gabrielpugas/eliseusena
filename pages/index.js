0850ec9f381df1056ef32672c156ca8d875966be
gabrielpugas <gabrielpugas@gmail.com>
2022-07-12 14:07:22 -0300

    menu inicial

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ function Home() {
   
   return (
     <div>
+      <div className="menu-superior">
+        <table className="navegacao-home" cellSpacing="0" cellSpacing-1=">
+          
+        </table>
+      </div>
+
+      </div>
+
       <h1>teste de formulário</h1>
       <h2>O site já está no ar, só falta fazer o site e colocar os vídeos e fotos que vamos fazer kkk</h2>
       <h3>
