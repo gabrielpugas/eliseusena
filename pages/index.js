92b28595ccc6c34d8c0a3167e9df189b006a4881
gabrielpugas <gabrielpugas@gmail.com>
2022-07-23 02:02:00 -0300

    Limpeza da página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
 function Home() {
   return (
     <div>
-      <h1>teste de formulário</h1>
-      <h2>
-        O site já está no ar, só falta fazer o site e colocar os vídeos e fotos
-        que vamos fazer kkk
-      </h2>
-      <h3>
-        Chama a sua véinha que vou chamar a minha, pra gente gravar as
-        propagandas
-      </h3>
-      <button onClick={ConfirmarPresença}>Confirmar presença</button>
+      <h1>Eliseu Sena</h1>
+      
+      <button onClick={alert=("Presença confirmada! Aqui é aroeira!")}>Confirmar presença</button>
     </div>
   )
 }
 
-function ConfirmarPresença() {
-  return alert('Presença confirmada! Aqui é aroeira!')
-}
+// function ConfirmarPresença() {
+//   return alert('Presença confirmada! Aqui é aroeira!')
+// }
 
 export default Home
